851ab0550cba75a099409c0571ca664d3954c78e
Correa <jcorrea7@dxc.com>
2020-09-20 04:05:27 -0300

    ESlint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/styles/App.scss'
+
+class HelloWorld extends React.Component{
+    render(){
+        return(
+            <h1>Holis</h1>
+        )
+    }
+}
+
+export default HelloWorld
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HelloWorld from './components/HelloWorld'
+
+const que = <HelloWorld/>
+const donde = document.getElementById('app')
+
+ReactDOM.render(que, donde)
